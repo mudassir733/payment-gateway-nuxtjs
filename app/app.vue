4368d3f28de51ea07b0c28bcabf47e76241e9ab9
mudassir733 <deadlool45@gmail.com>
2026-01-19 12:47:31 +0500

    first commit

---
+++ app/app.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1 class="text-gray-700 font-bold text-6xl">Hello</h1>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/fonts',
+    '@nuxt/hints',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/scripts',
+    '@nuxt/eslint',
+    '@nuxt/content',
+    '@nuxt/ui'
+  ],
+  css: ['~/assets/css/main.css']
+})
